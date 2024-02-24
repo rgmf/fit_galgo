dc3d294b06f34fa84f2c4c73312b3c4e4d50c939
Román Martínez <rgmf@riseup.net>
2024-02-24 19:12:26 +0100

    Release 0.2.12 ready

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fit_galgo",
-    version="0.2.11",
+    version="0.2.12",
     author="Román Martínez",
     author_email="rgmf@riseup.net",
     description="Descripción de tu proyecto",
